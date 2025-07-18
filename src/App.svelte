4c93401d1262b4d585dc5cd416db781be95907f1
Shivani <shdeo.learn@gmail.com>
2025-07-18 07:07:30 +0530

    Working about me with image

---
+++ src/App.svelte
@@ -0,0 +1,120 @@
+<script>
+  // document.body.style.backgroundColor="red";
+
+  //For background
+  let toggle = $state(false);
+  function changeBgColor() {
+    toggle = !toggle;
+
+    if (toggle == true) {
+      //document.body.style.animation = "change 10s infinite";
+      //  document.body.style.backgroundColor= "rgb(73, 190, 236)";
+      document.body.classList.add("animate-background");
+    } else {
+      document.body.classList.remove("animate-background");
+    }
+  }
+
+  import DisplayReco from "./lib/BookReco.svelte";
+</script>
+
+<h1>Shivani</h1>
+
+<button id="color" style="float: right" onclick={changeBgColor}>
+  {toggle == true
+    ? "Click to revert"
+    : "Click to view this page with my favourite background!"}</button
+>
+
+<br /><br />
+<h2 style="text-align: center;">Hello There!</h2>
+<p>I am an undergraduate student living in India</p>
+<p>Scroll to know more!</p>
+
+<div class="banner" id="codingBanner" style="text-decoration: dashed">
+  Coding <code>&lt;/&gt;</code>
+</div>
+<p>Kode With Klossy Website Development Scholar, 2021 &lt;br&gt;</p>
+<p>Kode With Klossy Data Science Scholar, 2024 &lt;br&gt;</p>
+<p>
+  Programming Languages (in decreasing order of proficiency):<large>
+    <large> C++, </large>
+  </large>
+  <large>C, </large><small
+    >Java, <small
+      >HTML, <small>
+        CSS, JavaScript, <small>Assembly, Python</small></small
+      ></small
+    ></small
+  >&lt;br&gt;
+</p>
+<p>I can code in more languages than I can speak! &lt;br&gt;</p>
+
+<div class="banner" style="text-decoration: dashed">Hobbies</div>
+<center>
+  <div class="container">
+    <img
+      src="pexels-element5-1370295.jpg"
+      alt="books"
+      style="width:100%;height:700px;margin:1%"
+    />
+    <div class="centered">
+      <p style="font-size: 50px;background-color:rgba(255, 255, 255, 0.801);">
+        READING <br /><small>
+          READING <br /><small>
+            Reading <br /><small>
+              Reading <br />
+              <small>
+                Reading <br /><small>
+                  Reading <br /><small> Reading </small></small
+                ></small
+              ></small
+            ></small
+          ></small
+        >
+      </p>
+    </div>
+  </div>
+</center>
+
+<p>It's reading all the way</p>
+<p>My favourite genre is science fiction</p>
+<p>I love writing as well, expecially haiku</p>
+
+<div class="banner">Random</div>
+<DisplayReco />
+
+<ul>
+  <li>I love reading and writing inspirational quotes</li>
+  <li>
+    Here's a <a
+      title="The Climb"
+      href="https://www.youtube.com/watch?v=NG2zyeVRcbs">song</a
+    > I like
+  </li>
+  <li>I like making lists</li>
+</ul>
+
+<div id="fancy" style="text-align: center">
+  <i
+    >I am passionate about many things, such as literature, coding and sports
+    &#10024; &#x2728;</i
+  >
+</div>
+<p id="hearts" style="color: goldenrod; font-size: 30px;">
+  &hearts; &hearts; &hearts; &hearts; &hearts; &hearts; &hearts; &hearts;
+  &hearts; &hearts; &hearts; &hearts; &hearts;
+</p>
+
+<style>
+  h1 {
+    color: #2c3e50;
+    font-family: Arial, sans-serif;
+  }
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: center;
+  }
+</style>
---
+++ src/lib/BookReco.svelte
@@ -0,0 +1,48 @@
+<script>
+  let books = [
+    "What Money Can't Buy",
+    "The Inimitable Jeeves",
+    "Percy Jackson series",
+    "Lab Girl",
+    "The Hitchhiker's Guide to the Galaxy",
+    "A Wrinkle in Time",
+    "Artemis Fowl",
+    "Wonder",
+    "Fintan Fedora: The World's Worst Explorer",
+    "Inkheart",
+    "A Brief History of Time",
+  ];
+
+  let random = $state();
+  let index = $derived(Math.floor(random));
+  let book = $derived(books[index]);
+
+  function displayBook() {
+    random = Math.random() * 11;
+  }
+</script>
+
+<!-- svelte-ignore a11y_mouse_events_have_key_events -->
+<!-- svelte-ignore a11y_distracting_elements -->
+<marquee onmouseover={this.stop()} onmouseout={this.start()}>
+  <button onclick={displayBook}>Click for a book recommendation</button>
+</marquee>
+<center><p>{book}</p></center>
+
+<style>
+  button:hover {
+    cursor: pointer;
+    background-color: rgba(152, 205, 245, 0.692);
+  }
+
+  button {
+    height: 3.2em;
+    font-size: 16px;
+    font-family: "Anonymous Pro", monospace;
+    font-weight: 400;
+    background-color: rgba(181, 215, 241, 0.692);
+    border-radius: 8px;
+    border-color: rgb(181, 215, 241);
+    margin: 1%;
+  }
+</style>
