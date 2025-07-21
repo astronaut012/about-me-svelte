4520d47e983e780dc7ef3a5ca739752423f8acf8
Shivani <shdeo.learn@gmail.com>
2025-07-21 17:29:45 +0530

    update base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/about-me-svelte',    // right here! your repository name
+  base: '/about-me-svelte/',    // right here! your repository name
   plugins: [svelte()],
 })
