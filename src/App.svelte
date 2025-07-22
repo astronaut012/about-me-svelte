5473514a5e099bb93a5c4e2eacfb3be769d5fc90
Shivani <shdeo.learn@gmail.com>
2025-07-22 16:47:13 +0530

    adjusted src

--- src/App.svelte
+++ src/App.svelte
@@ -55,7 +55,7 @@
   <img
     alt="books"
     style="width:100%;height:700px;margin:1%"
-    src="pexels-element5-1370295.jpg"
+    src="books.jpg"
   />
   <div class="centered">
     <p style="font-size: 50px;background-color:rgba(255, 255, 255, 0.801);">
